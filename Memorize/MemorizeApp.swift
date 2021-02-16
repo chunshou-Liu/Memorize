5bc1a1d0e4ae52c46c6cc7a6ac0331bb1f872cac
Chun-Shou <ms0727473@gmail.com>
2021-02-16 20:48:37 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by chunshou on 2021/2/16.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
